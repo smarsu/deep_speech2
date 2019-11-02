9e2f36da9e4bde070591bacfba8375929add65c6
smarsu <754247874@qq.com>
2019-11-02 19:16:38 +0800

    for test

--- networks/res_speech.py
+++ networks/res_speech.py
@@ -3,6 +3,16 @@ from torch import *
 import torch.nn as nn
 # import torchvision
 
+def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=dilation, groups=groups, bias=False, dilation=dilation)
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
 
 class Bottleneck(torch.nn.Module):
     expansion = 4
