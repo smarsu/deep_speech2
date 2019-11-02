dd25531a7da99b1997e3c675e6606fcc8017432e
smarsu <754247874@qq.com>
2019-11-03 00:00:40 +0800

    for test

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
         root = '/share/datasets/data_aishell'
         aishell = datasets.Aishell(root)
         # net = deep_speech2.DeepSpeech2(201, 4231)
-        net = ResSpeech()
+        net = deep_speech2.ResSpeech()
         model = model.SpeechRecognitionModel(net, deep_speech2.ctc_loss, model_name='res_speech')
         # model.train(aishell, epoch=1000000, batch_size=32, lr=0.1)  # 18
         model.train(aishell, epoch=1000000, batch_size=32, lr=0.1, params_path=None)
