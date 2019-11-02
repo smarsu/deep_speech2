7d92161643780b4f507534c9dd5a635efac49c10
smarsu <754247874@qq.com>
2019-11-02 19:29:45 +0800

    for test

--- networks/res_speech.py
+++ networks/res_speech.py
@@ -216,7 +216,7 @@ class ResSpeech(torch.nn.Module):
 if __name__ == '__main__':
     res_speech = ResSpeech()
     while True:
-        x = torch.rand(32, 3, 16000 * 10 // 10, 32)
+        x = torch.rand(32, 3, 16000 * 10 // 100, 32)
         x = res_speech(x)
         print(x.cpu().detach().numpy().shape)
 
