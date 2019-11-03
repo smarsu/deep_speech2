96dc28edc22f969aa32250703438e2cbca22bca7
smarsu <754247874@qq.com>
2019-11-03 15:14:52 +0800

    .

--- networks/shalow_speech.py
+++ networks/shalow_speech.py
@@ -5,6 +5,11 @@ class ShallowSpeech(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
+        self.conv = torch.nn.Conv2d(inchannels=96*2,
+                                    out_channels=96*2,
+                                    kernel_size=[1, 3],
+                                    stride=[1, 2])
+
         self.gru = torch.nn.GRU(input_size=96 * 2,
                                 hidden_size=96,
                                 num_layers=1,
@@ -16,6 +21,12 @@ class ShallowSpeech(torch.nn.Module):
 
 
     def forward(self, x):
+        for _ in range(5):
+            x = self.conv(x)
+
+        x = torch.squeeze(x)  # [32, 92 * 2, 16000 * 10 * 2 // 96 // 2]
+        x = torch.transpose(x, [0, 2, 1])
+
         x = self.gru(x)
         x = self.fc(x[0])
         return x
@@ -24,6 +35,6 @@ class ShallowSpeech(torch.nn.Module):
 if __name__ == '__main__':
     shallow_speech = ShallowSpeech().cuda()
     while True:
-        x = torch.rand(32, 16000 * 10 * 2 // 96 // 2, 96 * 2).cuda()
+        x = torch.rand(32, 96 * 2, 1, 16000 * 10 * 2 // 96 // 2).cuda()
         x = shallow_speech(x)
         print(x.cpu().detach().numpy().shape)
